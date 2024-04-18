f9cdde663b0f2255f97146976480aa89537baeaf
cyclops23 <anders.cassidy@dailypay.com>
2024-04-18 12:43:17 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-consul write config ext-dynamodb-service-defaults.hcl
+consul config write ext-dynamodb-service-defaults.hcl
 consul services register ext-dynamodb-service.hcl
 nomad job run ext-dynamodb-tgw-job.hcl
